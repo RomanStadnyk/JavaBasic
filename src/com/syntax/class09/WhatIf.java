4dde2fdbc732428ce42729f3485c2a89c16aca42
ridgeroman <ridgeroman.st@gmail.com>
2021-11-01 14:40:51 -0400

    adding class09

---
+++ src/com/syntax/class09/WhatIf.java
@@ -0,0 +1,16 @@
+package com.syntax.class09;
+
+public class WhatIf {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i=1;i<3; i++) {
+			for (int j=0;j<=2;j++) {
+				System.out.println("Hello");
+			}
+			System.out.println("Morning");
+			break;
+		}
+	}
+
+}
---
+++ src/com/syntax/class09/pattern.java
@@ -0,0 +1,22 @@
+package com.syntax.class09;
+
+public class pattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i=10;i>=1;i--) {
+			for (int j=1; j<=10; j++) {
+			System.out.print(" "+j);
+		}
+			System.out.println();
+	}
+		System.out.println("===========================================");
+		///////////////////////////////////////
+		for(int r=5; r>=1; r--) {
+			for(int c=1; c<=4; c++) {
+				System.out.print(r+" ");
+			}
+			System.out.println();
+		}
+	}
+}
