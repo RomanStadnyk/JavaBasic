97964e4e109e9f2498a551b4cda4fa9c1aadd729
ridgeroman <ridgeroman.st@gmail.com>
2021-12-14 19:47:18 -0500

    adding classes and HW028

---
+++ src/com/syntax/homework/class18/lkmkjjhgv.java
@@ -0,0 +1,12 @@
+package com.syntax.homework.class18;
+
+public class lkmkjjhgv {
+	
+	public static void main(String args[]) {
+		String str_Sample = "RockStar";
+		System.out.println("EndsWith character 'r': " + str_Sample.endsWith("r"));
+
+} 
+}
+
+
